664e91ba368422fa0aa990d614a9b2a5f0a58fa2
zeynepkaplan <zeynepkplnz@hotmail.com>
2024-07-28 01:29:44 +0300

    envanter slot eklemesi

---
+++ OUABootcamp2024/Assets/Scripts/InventoryPanel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPanel : MonoBehaviour
+{
+    [SerializeField] ItemContainer inventory;
+
+    private void Start()
+    {
+        Show();
+    }
+
+    private void Show()
+    {
+
+    }
+}
