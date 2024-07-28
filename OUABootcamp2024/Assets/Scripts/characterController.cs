0028dbff0c15ddf23625a1904dcfee0149aebf7a
zeynepkaplan <zeynepkplnz@hotmail.com>
2024-07-28 23:10:34 +0300

    .

--- OUABootcamp2024/Assets/Scripts/characterController.cs
+++ OUABootcamp2024/Assets/Scripts/characterController.cs
@@ -4,52 +4,15 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
-    public float speed = 0.0f;
-    Rigidbody2D r2d;
-    Animator _animator;
-    SpriteRenderer _spriteRenderer;
-    Vector3 charPos;
-    [SerializeField] GameObject _camera;
-
+    // Start is called before the first frame update
     void Start()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        r2d = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-        charPos = transform.position;
-    }
-
-    private void FixedUpdate()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
-        charPos = new Vector3(
-            charPos.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime),
-            charPos.y + (Input.GetAxis("Vertical") * speed * Time.deltaTime),
-            charPos.z
-        );
-        transform.position = charPos;
-
-        if (Input.GetAxis("Horizontal") == 0.0f && Input.GetAxis("Vertical") == 0.0f)
-        {
-            _animator.SetFloat("speed", 0.0f);
-        }
-        else
-        {
-            _animator.SetFloat("speed", 1.0f);
-        }
-
-        if (Input.GetAxis("Horizontal") > 0.01f)
-        {
-            _spriteRenderer.flipX = true;
-        }
-        else if (Input.GetAxis("Horizontal") < -0.01f)
-        {
-            _spriteRenderer.flipX = false;
-        }
+        
     }
 }
-
